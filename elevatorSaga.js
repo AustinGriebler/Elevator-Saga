4c0c0a95398738d7269367363205eec7ae7dac2d
Austin Griebler <31248971+AustinGriebler@users.noreply.github.com>
2022-09-27 14:55:02 -0500

    Update elevatorSaga.js
    Challenge #2

--- elevatorSaga.js
+++ elevatorSaga.js
@@ -1,15 +1,41 @@
 {
     init: function(elevators, floors) {
-        const elevator = elevators[0]; // Let's use the first elevator
+        const elevator = elevators[0];
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
+        elevator.on("floor_button_pressed", function(floorNum) {
+            elevator.goToFloor(floorNum);
+        })
+
+        elevator.on("passing_floor", function(floorNum, direction) {
+            console.log(elevator.loadFactor())
+            // Pick up passengers on the floor if there is room
+            if (elevator.loadFactor() <= 0.6) {    
+                 //if elevator going up or down, stop at floor to pick up passenger while going up or down if not full
+                if (floors[floorNum].buttonStates[direction] == "activated") { 
+                    elevator.goToFloor(floorNum, true); 
+                }
+            }
+            
+            const positionInQueue = elevator.destinationQueue.indexOf(floorNum)
+
+            // Stop at the floor first if it is in the queue
+            if (positionInQueue != -1) { 
+                //remove one floor from position in queue
+                elevator.destinationQueue.splice(positionInQueue, 1);
+                elevator.checkDestinationQueue();
+                elevator.goToFloor(floorNum, true); 
+            }
         });
 
+        floors.forEach(function(e) {
+            e.on("up_button_pressed", function(floorNum) {
+                elevator.goToFloor(floorNum.level);
+            })
+
+            e.on("down_button_pressed", function(floorNum) {
+                elevator.goToFloor(floorNum.level);
+            })
+        })
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
